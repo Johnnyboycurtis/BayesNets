f3d3e3157a05264ff414325c227278063588074e
Jonathan Navarrete <johnnyboycurtis@gmail.com>
2017-11-06 15:35:28 -0800

    adding a new test: start_train

--- TAN/src/TestCalcMutualInfo.py
+++ TAN/src/TestCalcMutualInfo.py
@@ -4,7 +4,8 @@ Created on Mon Nov  6 14:45:13 2017
 
 @author: jn107154
 """
-
+import os
+os.chdir(r"C:\Users\jn107154\BayesNets\TAN\src")
 from CalcMutualInfo import *
 
 
@@ -40,6 +41,16 @@ def testPairwiseCondProb(x, y):
 
 
 if __name__ == "__main__":
-    print("calculating mutual information")
-    mi = testCalcMutualInfo()
-    print(mi)
+    #print("calculating mutual information")
+    #mi = testCalcMutualInfo()
+    #print(mi)
+    
+    print("starting to train")
+    ## the following returns a dictionary
+    ## a key for each class
+    results = start_train("small-test.csv", "Class")
+    for i, frame in results.items():
+        print(f"DataFrame: {i}")
+        print(frame)
+    
+    
