16010e6bd24268a59e64a2b38dd1ea3ff967b300
Jonathan Navarrete <johnnyboycurtis@gmail.com>
2017-11-06 15:34:43 -0800

    finishing up a working prototype of step 1 in the algorithm

--- TAN/src/CalcMutualInfo.py
+++ TAN/src/CalcMutualInfo.py
@@ -9,18 +9,33 @@ import numpy as np
 def start_train(filename, class_col_name, sep = ","):
     df = pd.read_csv(filename, sep = sep)
     g = df.groupby(by = class_col_name)
-
+    ClassMats = {}
     for i, frame in g:
+        MutualInfMatrix = [] ## empty list to store pandas data frames
         frame.drop(labels = class_col_name, axis = 1, inplace=True) ## drop class column; gets in the way
         for j, xcol in frame.iteritems():
+            MutualInfo = {}
+            colnames = []
             for k, ycol in frame.iteritems():
-                if j != k:
+                colnames.append(k)
+                if j == k:
+                    MutualInfo[k] = 0
+                else:
                     xlist = xcol.tolist()
                     ylist = ycol.tolist()
-                    jointprob = PairWiseCondProb(xlist, ylist)
-                    MI = CalcMutualInfo(xlist, ylist, jointprob)
-
-
+                    xprobs = MarginalProb(xlist)
+                    yprobs = MarginalProb(ylist)
+                    jointprobs = PairWiseCondProb(xlist, ylist)
+                    MI = CalcMutualInfo(xprobs, yprobs, jointprobs)
+                    MutualInfo[k] = MI
+            #print(MutualInfo)
+            #print(f"index = {i} and colnames = {colnames}")
+            tmp = pd.DataFrame(data = MutualInfo, index = [j])
+            MutualInfMatrix.append(tmp)
+        MutualInfMatrix = pd.concat(objs = MutualInfMatrix)
+        ClassMats[i] = MutualInfMatrix
+    return ClassMats
+        
 
 
 def PairWiseCondProb(xlist, ylist):
