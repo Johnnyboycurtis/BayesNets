38abe1a89ffd386130d2a98b6400cb50f2297cd6
Jonathan Navarrete <johnnyboycurtis@gmail.com>
2017-11-08 09:13:23 -0800

    updated tests

--- TAN/src/TestAll.py
+++ TAN/src/TestAll.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     print("starting to train Graph")
     ## the following returns a dictionary
     ## a key for each class
-    results = start_train2("small-test.csv", "Class")
+    ## start_train2 return vertices, dictionary of data frames per class
+    vertices, results = start_train2("small-test.csv", "Class")
     for i, frame in results.items():
         print(f"DataFrame: {i}")
         print(frame)
@@ -25,18 +26,10 @@ if __name__ == "__main__":
         draw_graph(graph2, labels = labs)
         ## Build MST
         n = frame.shape[0]
-        g = Graph(3)  ## number of unique attributes
+        g = Graph(vertices, [])  ## number of unique attributes
         for ind,pair,mi in frame.itertuples():
             u,v = pair
             g.addEdge(u,v,mi) 
-            minst = g.KruskalMST(Maximum=False) ## return Minimum Spanning Tree
-            maxst = g.KruskalMST(Maximum=True) ## return Maximum Spanning Tree
-        """
-        STOP:
-            CURRENT ISSUE:
-                    if parent[i] == i:
-
-TypeError: list indices must be integers or slices, not str
-        """
-
-
+        minst = g.KruskalMST(maximum=False) ## return Minimum Spanning Tree
+        maxst = g.KruskalMST(maximum=True) ## return Maximum Spanning Tree
+        break
