0eb49985bd2bd8a0e4a1ced2e5a094470a079b90
Jonathan Navarrete <johnnyboycurtis@gmail.com>
2017-11-09 10:50:53 -0800

    updated module based on needs to calculate conditional probabilities

--- TAN/src/Probs.py
+++ TAN/src/Probs.py
@@ -1,26 +1,49 @@
 
-
+import numpy as np
 import itertools as it
 
-class Probs(object):
+class Probs():
     """Class representing probabilities"""
 
-    def __init__(self, xlist, ylist=None):
-        """Both xlist and ylist have to be lists"""
-        if ylist:
-            self.probs = self.PairWiseCondProb(xlist, ylist)
-        else:
-            self.probs = self.MarginalProb(xlist)
+    def __init__(self, ulist, vlist):
+        """Both ulist and vlist have to be lists"""
+        self.uprobs = self.MarginalProb(ulist)
+        self.vprobs = self.MarginalProb(vlist)
+        self.jointprobs = self.PairWiseCondProb(ulist, vlist)
+        self.reversed = False ## a flag checking if probs have been reversed
 
     def __repr__(self):
-        return "<Dict of Probs>"
+        return "<Class Probs: P(u), P(v), P(u,v)>"
     
     def __str__(self):
-        return "<Dict of Probs>"
+        if self.reversed:
+            out = "<P(v), P(u), P(v,u)>"
+        else:
+            out = "<P(u), P(v), P(u,v)>"
+        return out
     
     def __getitem__(self, key):
         return self.probs[key]
 
+    def __reverse__(self):
+        """
+        Graph module will sometimes reverse u,v --> v,u
+        Thus, I need to account for this change by
+        reversing positions of probabilities
+        """
+        uprobs = self.uprobs
+        vprobs = self.vprobs
+        ## switch
+        self.uprobs = vprobs 
+        self.vprobs = uprobs
+        jointprobs = {}
+        for key, val in self.jointprobs.items():
+            u,v = key
+            newkey = (v,u)
+            jointprobs[newkey] = val ## same val; switched key cuz Graph.py!
+        self.jointprobs = jointprobs
+        self.reversed = True
+    
     def keys(self):
         return self.probs.keys()
 
@@ -57,8 +80,55 @@ class Probs(object):
         return probs
 
 
-#sample = list("aaabbbccccccddddeeeeffhhhhhgggwww")
 
-#test = Probs(sample)
+    def CalcMutualInfo(self):
+        """
+        Calculate Mutual Information statistic
+        uprobs: dictionary of probabilities
+        vprobs: dictionary of probabilities
+        jointprobs: dictionary of probabilities
+        """
+        uprobs = self.uprobs
+        vprobs = self.vprobs
+        jointprobs = self.jointprobs
+        MI = [] ## collect Mutual Information
+        jointkeys = list(jointprobs.keys())
+        for uval, vval in jointkeys:
+            uprob = uprobs[uval]
+            vprob = vprobs[vval]
+            probxy = jointprobs[(uval, vval)]
+            I = probxy * np.log(probxy / (uprob * vprob))
+            MI.append(I)
+        MI = np.sum(MI)
+        return MI
+    
+    def ConditionalProb(self, u, v):
+        """
+        Calculate the Conditional Probability
+        u,v: values from the distributions of U and V
+        Calculates: P(V = v | U = u) = P(V,U)/P(U)
+        """
+        vprobs = self.vprobs
+        jointprobs = self.jointprobs
+        try:
+            PV = vprobs[v]
+            PUV = jointprobs[(u,v)]
+            condprob = PUV/PV ## P(U,V) / P(V)
+        except KeyError:
+            condprob = 0
+        return condprob
+            
+            
+        
+        
+        
+    
 
-#print(test)
+
+sample1 = list("aaabbbccccccddddeeeeffhhhhhgggwww")
+sample2 = list("aaabzzzccwwwccddddeeeeffwwwhhgggwww")
+
+test = Probs(sample1, sample2)
+test.ConditionalProb('w', 'z')
+
+print(test)
