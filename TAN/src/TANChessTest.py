b6da4ca6562e6131842afd14e805f686f9611b89
Jonathan Navarrete <johnnyboycurtis@gmail.com>
2017-11-10 09:48:16 -0800

    new tests for TAN module

---
+++ TAN/src/TANChessTest.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 10 09:16:10 2017
+
+@author: jn107154
+"""
+from tqdm import tqdm
+import pandas as pd
+from TAN import TAN
+import numpy as np
+
+## quick test ##
+#print("starting to train Graph")
+class_col_name = "ak"
+#traindf = pd.read_csv("../data/train-chess.csv")
+#testdf = pd.read_csv("../data/test-chess.csv")
+df = pd.read_csv("../data/chess.csv")
+n = df.shape[0]
+
+power = []
+for x in tqdm(range(10000)):
+    ind = np.random.rand(n) < 0.75
+    traindf = df.loc[ind]
+    testdf = df.loc[~ind]
+    model = TAN(dataframe = traindf, class_col_name = class_col_name)
+    
+    results = model.Predict(testdf)
+    accuracy = (testdf.ak.values == results.ak).mean()
+    power.append(accuracy)
+    print(f"TAN accuracy: {round(accuracy, 4)}")
+
+answer = sum(power)/len(power)
+print(f"final answer: {round(answer,4)}")
---
+++ TAN/src/TestTAN.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 10 08:26:20 2017
+
+@author: jn107154
+"""
+import pandas as pd
+from TAN import TAN
+import numpy as np
+
+## quick test ##
+#print("starting to train Graph")
+df = pd.read_csv("../data/Pima.tr.csv")
+n = df.shape[0]
+ind = np.random.rand(n) < 0.7
+df['bmi'] = df.bmi.apply(int) ## convert the float to integer
+class_col_name = "type"
+traindf = df.loc[ind]
+testdf = df.loc[~ind]
+model = TAN(dataframe = traindf, class_col_name = class_col_name)
+#myG  = toDiGraph(model.MST['No'])
+#test = find_root(myG, "age")
+
+results = model.Predict(testdf)
+accuracy = (testdf.type.values == results.type).mean()
+print(f"TAN accuracy: {round(accuracy, 4)}")
+
+
+
+
+
+
+## quick test ##
+#print("starting to train Graph")
+class_col_name = "IsDiabetic"
+traindf = pd.read_csv("../data/pima-train.csv")
+testdf = pd.read_csv("../data/pima-test.csv")
+model = TAN(dataframe = traindf, class_col_name = class_col_name)
+
+results = model.Predict(testdf)
+accuracy = (testdf.IsDiabetic.values == results.IsDiabetic).mean()
+print(f"TAN accuracy: {round(accuracy, 4)}")
