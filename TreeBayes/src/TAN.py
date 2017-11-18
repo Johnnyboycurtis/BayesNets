6fa89de7bcaec438ef81ddb8708a9e4e7768b34c
Jonathan <johnnyboycurtis@gmail.com>
2017-11-18 13:12:42 -0800

    new probs module for TAN algorithm

--- TreeBayes/src/TAN.py
+++ TreeBayes/src/TAN.py
@@ -10,7 +10,8 @@ Created on Wed Nov  8 10:28:01 2017
 import pandas as pd
 import itertools as it
 import numpy as np
-from Probs import Probs ## code for calculating joint/marginal probabilities
+#from Probs import Probs ## code for calculating joint/marginal probabilities
+from Probs2 import *
 #from Plot import PlotDiGraph, PlotNetwork ## plotting
 from tqdm import tqdm
 import networkx as nx
@@ -30,7 +31,7 @@ class TAN():
         self.Roots = self.SetRoots()
         self.MST = self.BuildMST()
         self.DAG = self.BuildDAG()
-        self.Models = self.BuildModel(dataframe)
+        #self.Models = self.BuildModel(dataframe) ## now a method
         
 
     def Priors(self, dataframe, class_col_name):
@@ -52,8 +53,8 @@ class TAN():
             colcombos = it.combinations(colnames, 2) ## will return tuples
             MutualInfo = []
             for u, v in colcombos:
-                ulist = frame[u].tolist()
-                vlist = frame[v].tolist()
+                ulist = frame[u] #.tolist() ## Probs2 takes a series
+                vlist = frame[v] #.tolist()
                 probs = Probs(ulist, vlist) ## calculates all probs
                 MI = probs.CalcMutualInfo() 
                 MutualInfo.append((u, v, MI)) ## no longer storing probs to save memory
@@ -69,6 +70,7 @@ class TAN():
         After calculating Mutual Info, use the top row to set the root for
         each MST
         Use the farthest edge as candidate for root
+        I need to test to see how this affects performance
         """
         Roots = {}
         ClassFrames = self.MIresults
@@ -122,6 +124,8 @@ class TAN():
                     v = v[0]
                 else:
                     v = None
+                ## U: Child
+                ## V: Parent, thus Parent can be None for Roots
                 edges.append((u,v))
             DAG[key] = edges
         return DAG
@@ -136,9 +140,9 @@ class TAN():
             edges = DAG[klass]
             MutualInfo = []
             for u, v in edges:
-                ulist = frame[u].tolist()
+                ulist = frame[u] #.tolist()
                 if v is not None:
-                    vlist = frame[v].tolist()
+                    vlist = frame[v] #.tolist()
                 else:
                     ## head node will have None
                     vlist = None
@@ -146,7 +150,7 @@ class TAN():
                 MutualInfo.append((u, v, probs)) ## no longer storing probs to save memory
             MutualInfMatrix = pd.DataFrame(MutualInfo, columns = ['U', 'V', "Probs"])
             ClassMats[klass] = MutualInfMatrix
-        return ClassMats
+        return TreeBayes(self.priors, ClassMats, self.class_col_name)
             
         
 
@@ -183,20 +187,60 @@ class TAN():
         return dfresult
 
 
+class TreeBayes():
+    def __init__(self, priors, TreeModels, class_col_name):
+        self.class_col_name = class_col_name
+        self.priors = priors
+        self.TreeModels = TreeModels
+
+    def Predict(self, newdf, log=False, progress_bar=False):
+        TreeProbs = self.TreeModels
+        newcols = newdf.columns.tolist()
+        if self.class_col_name in newcols:
+            newdf = newdf.drop(self.class_col_name, axis = 1)
+        results = []
+        rows = newdf.iterrows()
+        if progress_bar:
+            rows = tqdm(rows)
+        for i, row in newdf.iterrows():
+            sample = row.to_dict()
+            class_probs = {}
+            for class_name, treeframe in TreeProbs.items():
+                prior = self.priors[class_name]
+                LogPosterior = np.log(prior)
+                for ind, u, v, probs in treeframe.itertuples():
+                    if v is not None:
+                        pval = probs.ConditionalProb(sample[u], sample[v])
+                    else:
+                        pval = probs.PredMarginalProb(sample[u])
+                    logpval = np.log(pval)
+                    LogPosterior += logpval
+                if log:
+                    class_probs[class_name] = LogPosterior
+                else:
+                    class_probs[class_name] = np.exp(LogPosterior)
+            results.append(class_probs)
+        dfresult = pd.DataFrame(results)
+        dfresult[self.class_col_name] = dfresult.idxmax(axis=1)
+        return dfresult
+
+
+
 if __name__ == "__main__":
-    #pima = pd.read_csv("../data/pima.csv", dtype='str')
-    #class_col_name = "IsDiabetic"
+    pima = pd.read_csv("../data/pima.csv", dtype='str')
+     class_col_name = "IsDiabetic"
     #pima = pd.read_csv("../data/Pima.tr.csv")
     #class_col_name = "type"
     #pima = pd.read_csv("../data/chess.csv")
     #class_col_name = "ak"
-    pima = pd.read_csv("../data/train.csv") ## digit recognizer data
-    class_col_name = "label"
+    #pima = pd.read_csv("../data/train.csv") ## digit recognizer data
+    #class_col_name = "label"
     n = pima.shape[0]
     ind = np.random.rand(n) < 0.75
     traindf = pima.loc[ind]
     testdf = pima.loc[~ind]
-    tan = TAN(traindf, class_col_name)
-    results = tan.Predict(newdf = testdf)
+    tan = TAN(traindf, class_col_name) ## learns the tree structure
+    treebayes = tan.BuildModel(traindf) ## build the tree structure
+    results = treebayes.Predict(newdf = testdf)
     accuracy = np.mean(results[class_col_name] == testdf[class_col_name].values)
     print(accuracy)
