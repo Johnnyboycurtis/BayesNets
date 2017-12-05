3c0153093f10c65e44f1bdb20b94eafcae5140d2
Jonathan Navarrete <johnnyboycurtis@gmail.com>
2017-12-05 13:40:09 -0800

    testing for FAN

--- NaiveBayesNets/tests/TAN/TestTreeNB.py
+++ NaiveBayesNets/tests/TAN/TestTreeNB.py
@@ -4,26 +4,23 @@
 
 
 import sys
-sys.path.append("/home/jonathan/BayesNets/NaiveBayesNets/FAN/treenb")
+sys.path.append("C:/Users/jn107154/BayesNets/")
 import pandas as pd
-import TAN
+from NaiveBayesNets import TAN
 import numpy as np
 
 ## quick test ##
 #np.random.seed(678)
-pima = pd.read_csv("/home/jonathan/BayesNets/NaiveBayesNets/data/chess.csv")
-class_col_name = "ak"
-n = pima.shape[0]
+#df = pd.read_csv("C:/Users/jn107154/BayesNets/NaiveBayesNets/data/chess.csv")
+#class_col_name = "ak"
+df = pd.read_csv("C:/Users/jn107154/BayesNets/NaiveBayesNets/data/balance-scale.csv")
+class_col_name = "balance"
+n = df.shape[0]
 ind = np.random.rand(n) < 0.75
-traindf = pima.loc[ind]
-testdf = pima.loc[~ind]
+traindf = df.loc[ind]
+testdf = df.loc[~ind]
 discretetan = TAN.TreeNB(traindf, class_col_name) ## learns the tree structure
 treebayes = discretetan.BuildModel(traindf) ## build the tree structure
 results = treebayes.Predict(newdf = testdf)
 accuracy = np.mean(results[class_col_name] == testdf[class_col_name].values)
 print(accuracy)
-
-
-
-
-
