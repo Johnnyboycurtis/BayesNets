33caf292e75f3102962689c27fb402ac403bd998
Jonathan Navarrete <johnnyboycurtis@gmail.com>
2017-12-05 16:27:03 -0800

    adding method to Discretize class

--- NaiveBayesNets/FAN/kdebayes/Discretize.py
+++ NaiveBayesNets/FAN/kdebayes/Discretize.py
@@ -26,15 +26,44 @@ def discretize(data, density = True):
 class Discretize():
 
     def __init__(self, data):
+        """
+        Discretization is the process of converting continuous random variables
+        to discrete random variables. This method uses a histogram (i.e. np.histogram)
+        to bin values.
+
+        This class returns the histogram bin `(a,b)` as discretized values.
+        Additionally, you can use the method `density` to get the univariate density value.
+
+        ## test data
+        >>> np.random.seed(123)
+        >>> x = np.random.rand(1000)
+
+        >>> out = discretize(x)
+
+        >>> test = Discretize(x)
+
+        >>> print(test)
+
+        >>> print(test[0.5])
+
+        >>> print(test[0.1])
+        """
         self.length = len(data)
         self.histogram = discretize(data)
-    
-    def __getitem__(self, key):
+
+    def density(self, key):
         hist = self.histogram
-        for a,b, pval in hist:
+        for a, b, pval in hist:
             if a <= key and key < b:
                 #print("found!:",pval)
                 return pval
+    
+    def __getitem__(self, key):
+        hist = self.histogram
+        for a, b, pval in hist:
+            if a <= key and key < b:
+                return (a,b)
+    
 
 
 """
